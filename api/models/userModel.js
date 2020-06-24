e1914162400622812ca08a919a859fa0cc51f5ef
ManikMaharjan <manikmaharjan1@gmail.com>
2020-06-24 17:38:23 +0545

    email validation uprdrade

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
 email:{
     type : String, 
-    required :true
+    required :true,
+    unique: true,
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     
             
 },
